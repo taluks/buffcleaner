58ab11785d213c286c7a007b9ec48f5e1a1d0794
taluks <taluks@mail.ru>
2019-06-24 20:07:07 +0300

    add system tray icon

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,60 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    createActions();
+    createTrayIcon();
+
+    trayIcon->show();
+
+    setWindowTitle(tr("Buff Cleaner"));
+    setFixedSize(width(), height());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setVisible(bool visible)
+{
+    restoreAction->setEnabled(isMaximized() || !visible);
+    QMainWindow::setVisible(visible);
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    if (trayIcon->isVisible()) {
+        QMessageBox::information(this, tr("Systray"),
+                                 tr("The program will keep running in the "
+                                    "system tray. To terminate the program, "
+                                    "choose <b>Quit</b> in the context menu "
+                                    "of the system tray entry."));
+        hide();
+        event->ignore();
+    }
+}
+
+void MainWindow::createActions()
+{
+    restoreAction = new QAction(tr("&Open"), this);
+    connect(restoreAction, SIGNAL(triggered()), this, SLOT(showNormal()));
+
+    quitAction = new QAction(tr("&Quit"), this);
+    connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+}
+
+void MainWindow::createTrayIcon()
+{
+    trayIconMenu = new QMenu(this);
+    trayIconMenu->addAction(restoreAction);
+    trayIconMenu->addSeparator();
+    trayIconMenu->addAction(quitAction);
+
+    QIcon icon = QIcon(":/assets/trash.png");
+    trayIcon = new QSystemTrayIcon(this);
+    trayIcon->setContextMenu(trayIconMenu);
+    trayIcon->setIcon(icon);
+
+    setWindowIcon(icon);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,14 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSystemTrayIcon>
+#include <QMessageBox>
+#include <QCloseEvent>
+#include <QGroupBox>
+#include <QCheckBox>
+#include <QLabel>
+#include <QComboBox>
+#include <QHBoxLayout>
 
 namespace Ui {
 class MainWindow;
@@ -13,10 +21,22 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+    void setVisible(bool visible);
+    void createActions();
     ~MainWindow();
-
+protected:
+    void closeEvent(QCloseEvent *event);
 private:
     Ui::MainWindow *ui;
+
+    QSystemTrayIcon *trayIcon;
+    QMenu *trayIconMenu;
+
+    QAction *restoreAction;
+    QAction *quitAction;
+
+    void createIconGroupBox();
+    void createTrayIcon();
 };
 
 #endif // MAINWINDOW_H
